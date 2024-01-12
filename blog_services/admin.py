1506792fb12f89e9a64e6b3afbf6701cab98f659
Numan Aslam <unitednuman@gmail.com>
2024-01-12 17:18:16 +0500

    third lecture

--- blog_services/admin.py
+++ blog_services/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Articles,Publisher, Reporter
 
 # Register your models here.
+admin.site.register(Articles)
+admin.site.register(Publisher)
+admin.site.register(Reporter)
--- blog_services/models.py
+++ blog_services/models.py
@@ -8,3 +8,18 @@ class Reporter(models.Model):
     last_name = models.CharField(max_length=100, null=False, blank=False, default="Anonymous")
     email = models.EmailField(null=False, blank=False)
     phone_number = models.IntegerField(blank=False)
+
+
+class Publisher(models.Model):
+    company_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    reporter = models.ForeignKey(to=Reporter, on_delete=models.CASCADE)
+    publisher = models.ManyToManyField(to=Publisher)
